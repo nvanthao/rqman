8d5b43a22bb3241c014db39bd5c0d85e3959123a
Gerard Nguyen <gerard@replicated.com>
2025-01-06 16:01:28 +1100

    use env variable for talking to rqlite api

--- pages/api/[...all].ts
+++ pages/api/[...all].ts
@@ -1,3 +1,4 @@
+import { Buffer } from "buffer";
 import { IncomingMessage } from "http";
 import httpProxy from "http-proxy";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -12,12 +13,24 @@ export const config = {
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const rqliteHost = process.env.RQLITE_HOST || "http://localhost:4001";
+  const rqliteUsername = process.env.RQLITE_USERNAME || "kotsadm";
+  const rqlitePassword = process.env.RQLITE_PASSWORD;
+
   return new Promise((resolve, reject) => {
     const proxy: httpProxy = httpProxy.createProxy();
 
     proxy
       .once("proxyReq", (proxyReq, req, res, options) => {
-        proxyReq.setHeader("Authorization", String(req.headers["x-rqlite-authorization"]));
+        
+        // use environment variables first
+        if (rqliteUsername && rqlitePassword) {
+          const authHeader = Buffer.from(`${rqliteUsername}:${rqlitePassword}`).toString("base64");
+          proxyReq.setHeader("Authorization", `Basic ${authHeader}`);
+        } else {
+          // fallback to client-provided header
+          proxyReq.setHeader("Authorization", String(req.headers["x-rqlite-authorization"]));
+        }
+
         if(proxyReq.path && req.url) {
           proxyReq.path = req.url.replace("/api/rqlite", "");
         }
